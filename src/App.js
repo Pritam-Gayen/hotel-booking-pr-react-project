ee6f0f744bff75df911d1831a80e771cecc2152c
Pritam-Gayen <pritsaccount@gmail.com>
2023-03-20 14:22:06 +0530

    installed react icons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ function App() {
   return (
     <>
       hotel booking app by pritam and ranjeet
+      <br></br>
       installed mui
+      <br></br>
+      installed react icons
     </>
   );
 }
